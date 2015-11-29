cabe7fde26aebbd65a7a2138a5a417530f13feb0
YuliyaYaroshuk <yulia.yaroshuk@gmail.com>
2015-11-29 18:50:40 +0300

    Фикстура сделана общей для всех

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,16 +1,6 @@
 # -*- coding: utf-8 -*-
-
-import pytest
-
-from fixture.application_contact import Application_Cont
 from model.contact import Contact
 
-
-@pytest.fixture
-def app(request):
-    fixture = Application_Cont()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 def test_add_contact(app):
     app.session.login( username="admin",password="secret")
